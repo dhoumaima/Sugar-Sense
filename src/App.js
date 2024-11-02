3b90b38d42b218fe939a44aab24b6de9616586e4
dhoumaima <dhoumaima077@gmail.com>
2024-11-02 14:38:13 +0100

    Home Page Structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2 className="title">SugarSense</h2>
+
+      <p className="Description">Are you concerned about your health? <br/> Discover if you might be at risk for diabetes with our quick and easy online test!
+      <br/> Understanding your blood sugar levels is a crucial step in maintaining your overall well-being. 
+      <br/>Our user-friendly platform provides you with a simple questionnaire designed to assess your risk factors. 
+      <br/>Don’t wait until it’s too late—take control of your health today <br/> and find out if you should consult a healthcare professional. 
+     </p>
+      <p className="P1">Your journey towards a healthier life starts here!</p>
       
+      <button className="button">Démarrer le test</button>
     </div>
   );
 }
